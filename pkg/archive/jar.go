6dd67d5c7ac4db112f3419b5132d8f80a22cbe65
Aman Sharma <mannu.poski10@gmail.com>
2025-04-06 13:26:18 +0200

    fix: add stabilizer to top level

--- pkg/archive/jar.go
+++ pkg/archive/jar.go
@@ -16,6 +16,7 @@ var AllJarStabilizers []Stabilizer = []Stabilizer{
 	StableJARBuildMetadata,
 	StableJAROrderOfAttributeValues,
 	StableGitProperties,
+	StablePomProperties,
 }
 
 var StableJARBuildMetadata = ZipEntryStabilizer{
